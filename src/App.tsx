449a4b1cfb79def32ed4e60bbb038b5fef1e2127
Callum Douglas <callum@mondago.com>
2020-03-04 16:28:38 +0000

    use state machine to power a computer in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from "react"
-import logo from "./logo.svg"
+import { useMachine } from "@xstate/react"
 import "./App.css"
+import computerStartup from "./machines/computerStartup"
 
 function App() {
+    const [state, send] = useMachine(computerStartup)
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            {state.value}
+            <button onClick={() => send("POWER_BUTTON_PUSHED")}>power</button>
+            {state.matches("loginScreen") ? <button onClick={() => send("LOGIN")}>login</button> : ""}
+            {state.matches("desktop") ? <button onClick={() => send("LOGOUT")}>logout</button> : ""}
         </div>
     )
 }
